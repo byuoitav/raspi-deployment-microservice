76976ea2daaa2272dac540204f0ce464b1f04526
Jesse Millar <hellojessemillar@gmail.com>
2016-09-09 12:55:05 -0600

    Better JSON responses

--- handlers/webhook.go
+++ handlers/webhook.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/byuoitav/raspi-deployment-microservice/helpers"
+	"github.com/jessemillar/jsonresp"
 	"github.com/labstack/echo"
 )
 
@@ -15,13 +16,13 @@ func Webhook(context echo.Context) error {
 	repo := circlePayload{}
 	err := context.Bind(&repo)
 	if err != nil {
-		return context.String(http.StatusBadRequest, err.Error())
+		return jsonresp.New(context, http.StatusBadRequest, err.Error())
 	}
 
 	response, err := helpers.Deploy(repo.Name)
 	if err != nil {
-		return context.String(http.StatusBadRequest, err.Error())
+		return jsonresp.New(context, http.StatusBadRequest, err.Error())
 	}
 
-	return context.JSON(http.StatusOK, response)
+	return jsonresp.New(context, http.StatusOK, response)
 }
