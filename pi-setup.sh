710a98cc49a4e06801af1ea4a485ebbd019a4bc1
Jesse Millar <hellojessemillar@gmail.com>
2016-10-07 16:19:21 -0600

    Update pi-setup.sh

--- pi-setup.sh
+++ pi-setup.sh
@@ -24,7 +24,7 @@ cp -r /etc/xdg/awesome/ ~/.config/awesome/
 echo "\nawful.util.spawn_with_shell('chromium-browser --kiosk http://localhost:8888')\n" >> ~/.config/awesome/rc.lua
 
 # Install an ARM-specific Docker version
-curl -sSL http://downloads.hypriot.com/docker-hypriot_1.10.3-1_armhf.deb > /tmp/docker-hypriot_1.10.3- 1_armhf.deb
+curl -sSL http://downloads.hypriot.com/docker-hypriot_1.10.3-1_armhf.deb > /tmp/docker-hypriot_1.10.3-1_armhf.deb
 dpkg -i /tmp/docker-hypriot_1.10.3-1_armhf.deb
 rm -f /tmp/docker-hypriot_1.10.3-1_armhf.deb
 sh -c 'usermod -aG docker $SUDO_USER'
