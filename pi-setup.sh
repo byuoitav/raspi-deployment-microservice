b2997cc22d2400f3c3b9b4179c30316729b868d0
Jesse Millar <hellojessemillar@gmail.com>
2016-11-08 09:28:23 -0700

    Updating file locations

--- pi-setup.sh
+++ pi-setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Set the proper keyboard layout
-curl https://raw.githubusercontent.com/byuoitav/raspi-deployment-microservice/master/keyboard > /etc/default/keyboard
+curl https://raw.githubusercontent.com/byuoitav/raspi-deployment-microservice/master/files/keyboard > /etc/default/keyboard
 
 # Update everything
 apt-get update
@@ -16,7 +16,7 @@ apt-get install -y awesome
 apt-get install -y chromium-browser
 
 # Make `startx` result in starting the Awesome window manager
-curl https://raw.githubusercontent.com/byuoitav/raspi-deployment-microservice/master/xinitrc > /home/pi/.xinitrc
+curl https://raw.githubusercontent.com/byuoitav/raspi-deployment-microservice/master/files/xinitrc > /home/pi/.xinitrc
 chmod +x /home/pi/.xinitrc
 chown pi /home/pi/.xinitrc
 chgrp pi /home/pi/.xinitrc
@@ -40,11 +40,11 @@ systemctl enable docker.service
 
 # Enable autologin
 mkdir -pv /etc/systemd/system/getty@tty1.service.d/
-curl https://raw.githubusercontent.com/byuoitav/raspi-deployment-microservice/master/autologin.conf > /etc/systemd/system/getty@tty1.service.d/autologin.conf
+curl https://raw.githubusercontent.com/byuoitav/raspi-deployment-microservice/master/files/autologin.conf > /etc/systemd/system/getty@tty1.service.d/autologin.conf
 systemctl enable getty@tty1.service
 
 # Make X start on login
-curl https://raw.githubusercontent.com/byuoitav/raspi-deployment-microservice/master/bash_profile > /home/pi/.bash_profile
+curl https://raw.githubusercontent.com/byuoitav/raspi-deployment-microservice/master/files/bash_profile > /home/pi/.bash_profile
 chown pi /home/pi/.bash_profile
 chgrp pi /home/pi/.bash_profile
 
