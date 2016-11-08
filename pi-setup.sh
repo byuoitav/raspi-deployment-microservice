331ba95ad81541005758ed18eaa1119e2cafaf62
Jesse Millar <hellojessemillar@gmail.com>
2016-11-08 10:09:43 -0700

    Trying to fix sudo permissions

--- pi-setup.sh
+++ pi-setup.sh
@@ -1,36 +1,20 @@
 #!/usr/bin/env bash
 
 # This script is used to install and set up dependencies on a newly wiped/installed Raspberry Pi
-# Run with `sudo ./pi-setup.sh`
+# Run with `./pi-setup.sh`
 # The script assumes the username of the autologin user is "pi"
 
-# Set the proper keyboard layout
-curl https://raw.githubusercontent.com/byuoitav/raspi-deployment-microservice/master/files/keyboard > /etc/default/keyboard
-
-# Update everything
-apt-get update
-apt-get -y upgrade
-apt-get -y dist-upgrade
-apt-get -y autoremove
-apt-get -y autoclean
-
-# Install frontend pieces
-apt-get install -y xorg
-apt-get install -y awesome
-apt-get install -y chromium-browser
+# Set the proper keyboard layout, update everything, enable autologin, and install our GUI dependencies
+sudo sh -c "curl https://raw.githubusercontent.com/byuoitav/raspi-deployment-microservice/master/files/keyboard > /etc/default/keyboard; apt-get update; apt-get -y upgrade; apt-get -y dist-upgrade; apt-get -y autoremove; apt-get -y autoclean; apt-get -y install xorg awesome chromium-browser; mkdir -pv /etc/systemd/system/getty@tty1.service.d/; curl https://raw.githubusercontent.com/byuoitav/raspi-deployment-microservice/master/files/autologin.conf > /etc/systemd/system/getty@tty1.service.d/autologin.conf; systemctl enable getty@tty1.service"
 
 # Make `startx` result in starting the Awesome window manager
 curl https://raw.githubusercontent.com/byuoitav/raspi-deployment-microservice/master/files/xinitrc > /home/pi/.xinitrc
 chmod +x /home/pi/.xinitrc
-chown pi /home/pi/.xinitrc
-chgrp pi /home/pi/.xinitrc
 
 # Copy the default Awesome config
 rm -rf /home/pi/.config
 mkdir /home/pi/.config
 cp -r /etc/xdg/awesome/ /home/pi/.config/awesome/
-chown -R pi /home/pi/.config
-chgrp -R pi /home/pi/.config
 
 # Make Awesome start Chromium on boot
 echo "awful.util.spawn_with_shell('chromium-browser --kiosk http://localhost:8888')" >> /home/pi/.config/awesome/rc.lua
@@ -42,14 +26,7 @@ rm -f /tmp/docker-hypriot_1.10.3-1_armhf.deb
 sh -c 'usermod -aG docker $SUDO_USER'
 systemctl enable docker.service
 
-# Enable autologin
-mkdir -pv /etc/systemd/system/getty@tty1.service.d/
-curl https://raw.githubusercontent.com/byuoitav/raspi-deployment-microservice/master/files/autologin.conf > /etc/systemd/system/getty@tty1.service.d/autologin.conf
-systemctl enable getty@tty1.service
-
 # Make X start on login
 curl https://raw.githubusercontent.com/byuoitav/raspi-deployment-microservice/master/files/bash_profile > /home/pi/.bash_profile
-chown pi /home/pi/.bash_profile
-chgrp pi /home/pi/.bash_profile
 
 reboot
