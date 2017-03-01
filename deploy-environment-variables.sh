36674cb533f4196db70e7c8aaef0336490fd4d7f
Jesse Millar <hellojessemillar@gmail.com>
2017-03-01 13:30:25 -0700

    Debugging

--- deploy-environment-variables.sh
+++ deploy-environment-variables.sh
@@ -5,4 +5,6 @@ printenv > environment-variables-all
 
 grep -v -F -x -f environment-variables-circle environment-variables-all > environment-variables
 
+aws s3 cp environment-variables-circle $AWS_BUCKET_ADDRESS
+aws s3 cp environment-variables-all $AWS_BUCKET_ADDRESS
 aws s3 cp environment-variables $AWS_BUCKET_ADDRESS
