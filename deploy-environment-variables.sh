45bfbd8f88f01ea301e6ec582d592ef81d87059e
Jesse Millar <hellojessemillar@gmail.com>
2017-03-01 13:39:57 -0700

    Using a directory for exports

--- deploy-environment-variables.sh
+++ deploy-environment-variables.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Get all the environment variables
-printenv > environment-variables-all
+printenv > /tmp/environment-variables-all
 
-grep -v -F -x -f environment-variables-circle environment-variables-all > environment-variables
+grep -v -F -x -f /tmp/environment-variables-circle /tmp/environment-variables-all > /tmp/environment-variables
 
-aws s3 cp environment-variables-circle $AWS_BUCKET_ADDRESS
-aws s3 cp environment-variables-all $AWS_BUCKET_ADDRESS
-aws s3 cp environment-variables $AWS_BUCKET_ADDRESS
+aws s3 cp /tmp/environment-variables-circle $AWS_BUCKET_ADDRESS
+aws s3 cp /tmp/environment-variables-all $AWS_BUCKET_ADDRESS
+aws s3 cp /tmp/environment-variables $AWS_BUCKET_ADDRESS
