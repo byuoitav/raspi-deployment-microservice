0edaea28106839547ecbf1e7b87c21c1b5d7371d
Jesse Millar <hellojessemillar@gmail.com>
2017-03-01 14:59:23 -0700

    Learning more about sed

--- deploy-environment-variables.sh
+++ deploy-environment-variables.sh
@@ -7,10 +7,8 @@ printenv > /tmp/environment-variables-all
 grep -v -F -x -f /tmp/environment-variables-circle /tmp/environment-variables-all > /tmp/environment-variables
 
 # Remove remnant variables from Circle
-sed '/GOPATH/d' environment-variables > environment-variables
-sed '/SHLVL/d' environment-variables > environment-variables
-sed '/PWD/d' environment-variables > environment-variables
+sed '/GOPATH/d' environment-variables > tmpfile; mv tmpfile environment-variables
+sed '/SHLVL/d' environment-variables > tmpfile; mv tmpfile environment-variables
+sed '/PWD/d' environment-variables > tmpfile; mv tmpfile environment-variables
 
-aws s3 cp /tmp/environment-variables-circle $AWS_BUCKET_ADDRESS
-aws s3 cp /tmp/environment-variables-all $AWS_BUCKET_ADDRESS
 aws s3 cp /tmp/environment-variables $AWS_BUCKET_ADDRESS
