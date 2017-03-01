20f9f4964d0d9d7d32e4030edd91da8f47d5c300
Jesse Millar <hellojessemillar@gmail.com>
2017-03-01 14:28:29 -0700

    Removing remnant environment variables from Circle

--- deploy-environment-variables.sh
+++ deploy-environment-variables.sh
@@ -3,7 +3,13 @@
 # Get all the environment variables
 printenv > /tmp/environment-variables-all
 
+# Find only the custom variables
 grep -v -F -x -f /tmp/environment-variables-circle /tmp/environment-variables-all > /tmp/environment-variables
+
+# Remove remnant variables from Circle
+sed '/GOPATH/d' environment-variables > environment-variables
+sed '/SHLVL/d' environment-variables > environment-variables
+sed '/PWD/d' environment-variables > environment-variables
 
 aws s3 cp /tmp/environment-variables-circle $AWS_BUCKET_ADDRESS
 aws s3 cp /tmp/environment-variables-all $AWS_BUCKET_ADDRESS
