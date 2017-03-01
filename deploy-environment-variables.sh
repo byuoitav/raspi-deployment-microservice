aa6c6d693e6b1552e51aa13e085b61ba7aabfc0e
Jesse Millar <hellojessemillar@gmail.com>
2017-03-01 15:08:08 -0700

    I'm a foolish man

--- deploy-environment-variables.sh
+++ deploy-environment-variables.sh
@@ -7,8 +7,8 @@ printenv > /tmp/environment-variables-all
 grep -v -F -x -f /tmp/environment-variables-circle /tmp/environment-variables-all > /tmp/environment-variables
 
 # Remove remnant variables from Circle
-sed '/GOPATH/d' environment-variables > tmpfile; mv tmpfile environment-variables
-sed '/SHLVL/d' environment-variables > tmpfile; mv tmpfile environment-variables
-sed '/PWD/d' environment-variables > tmpfile; mv tmpfile environment-variables
+sed '/GOPATH/d' /tmp/environment-variables > /tmp/tmpfile; mv /tmp/tmpfile /tmp/environment-variables
+sed '/SHLVL/d' /tmp/environment-variables > /tmp/tmpfile; mv /tmp/tmpfile /tmp/environment-variables
+sed '/PWD/d' /tmp/environment-variables > /tmp/tmpfile; mv /tmp/tmpfile /tmp/environment-variables
 
 aws s3 cp /tmp/environment-variables $AWS_BUCKET_ADDRESS
