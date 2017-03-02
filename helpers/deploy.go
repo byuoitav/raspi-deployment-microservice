c85a06ab41ce5309e2ce49aafb32459225787320
Jesse Millar <hellojessemillar@gmail.com>
2017-03-02 16:52:32 -0700

    Pushing raspi-tp to the Pi's

--- helpers/deploy.go
+++ helpers/deploy.go
@@ -121,7 +121,7 @@ func SendCommand(hostname string, fileName string) error {
 	defer magicSession.Close()
 	log.Printf("SSH session established.")
 
-	err = magicSession.Start("sh -c 'curl https://raw.githubusercontent.com/byuoitav/raspi-deployment-microservice/master/docker-compose.yml --output /tmp/docker-compose.yml && curl " + os.Getenv("RASPI_DEPLOYMENT_MICROSERVICE_ADDRESS") + "/" + fileName + " --output /home/pi/.environment-variables && source ~/.bashrc && docker-compose -f /tmp/docker-compose.yml up -d &'")
+	err = magicSession.Start("sh -c 'curl https://raw.githubusercontent.com/byuoitav/raspi-deployment-microservice/master/docker-compose.yml --output /tmp/docker-compose.yml && curl " + os.Getenv("RASPI_DEPLOYMENT_MICROSERVICE_ADDRESS") + "/" + fileName + " --output /home/pi/.environment-variables && source ~/.bashrc && docker-compose -f /tmp/docker-compose.yml pull && docker-compose -f /tmp/docker-compose.yml up -d --remove-orphans'")
 	if err != nil {
 		return err
 	}
