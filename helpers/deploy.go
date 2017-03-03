5d87a22b5631f2ecc9b26ea9ed8999bc2d84d493
Jesse Millar <hellojessemillar@gmail.com>
2017-03-03 11:29:51 -0700

    Better error reporting and logging

--- helpers/deploy.go
+++ helpers/deploy.go
@@ -1,11 +1,13 @@
 package helpers
 
 import (
+	"bytes"
 	"encoding/json"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/byuoitav/authmiddleware/bearertoken"
 
@@ -43,33 +45,10 @@ func Deploy() (string, error) {
 	}
 
 	for i := range allDevices {
-		log.Printf("%+v", allDevices[i])
-
-		go SendCommand(allDevices[i].Address, fileName)
-		// err := SendCommand(allDevices[i].Address, fileName)
-		/*
-			if err != nil {
-				log.Printf("Error updating %s at %s", allDevices[i].Name, allDevices[i].Address)
-
-				log.Printf("Sending error to %s\n", os.Getenv("ELK_ADDRESS"))
-
-				report := elkReport{Hostname: allDevices[i].Address, Timestamp: time.Now().Format(time.RFC3339), Action: "Deployment failed to start: " + err.Error()}
-				data, err := json.Marshal(&report)
-				if err != nil {
-					log.Printf("Error sending error report.")
-					continue
-				}
-
-				_, err = http.Post(os.Getenv("ELK_ADDRESS"), "application/json", bytes.NewBuffer(data))
-				if err != nil {
-					log.Printf("Error sending error report: %s.", err.Error())
-				}
-				continue
-			}
-		*/
+		go SendCommand(allDevices[i].Address, fileName) // Start an update for each Pi
 	}
 
-	log.Printf("Deployment finished.")
+	log.Printf("Deployment started")
 	return "Deployment started", nil
 }
 
@@ -100,38 +79,59 @@ func GetDevices() ([]device, error) {
 		return []device{}, err
 	}
 
-	log.Printf("%+v", allDevices)
+	log.Printf("All devices from database: %+v", allDevices)
 
 	return allDevices, nil
+}
+
+func reportToELK(hostname string, err error) {
+	log.Printf("Sending error to %s\n", os.Getenv("ELK_ADDRESS"))
+
+	report := elkReport{Hostname: hostname, Timestamp: time.Now().Format(time.RFC3339), Action: "Deployment failed to start: " + err.Error()}
+	data, err := json.Marshal(&report)
+	if err != nil {
+		log.Printf("Error sending error report for %s to %s", hostname, os.Getenv("ELK_ADDRESS"))
+	}
+
+	_, err = http.Post(os.Getenv("ELK_ADDRESS"), "application/json", bytes.NewBuffer(data))
+	if err != nil {
+		log.Printf("Error sending the following error report for %s to %s: %s.", hostname, os.Getenv("ELK_ADDRESS"), err.Error())
+	}
 }
 
 func SendCommand(hostname string, fileName string) error {
 	connection, err := ssh.Dial("tcp", hostname+":22", sshConfig)
 	if err != nil {
+		log.Printf("Error dialing %s: %s", hostname, err.Error())
+		reportToELK(hostname, err)
 		return err
 	}
 
-	log.Printf("TCP connection established.")
+	log.Printf("TCP connection established to %s", hostname)
 	defer connection.Close()
 
 	magicSession, err := connection.NewSession()
 	if err != nil {
+		log.Printf("Error starting a session with %s: %s", hostname, err.Error())
+		reportToELK(hostname, err)
 		return err
 	}
 
 	//defer magicSession.Close()
-	log.Printf("SSH session established.")
+	log.Printf("SSH session established with %s", hostname)
 
-	longCommand := "sh -c 'curl https://raw.githubusercontent.com/byuoitav/raspi-deployment-microservice/master/docker-compose.yml --output /tmp/docker-compose.yml && curl " + os.Getenv("RASPI_DEPLOYMENT_MICROSERVICE_ADDRESS") + "/" + fileName + " --output /home/pi/.environment-variables && . ~/.bashrc && docker-compose -f /tmp/docker-compose.yml pull && docker-compose -f /tmp/docker-compose.yml up -d --remove-orphans'"
+	longCommand := "sh -c 'curl https://raw.githubusercontent.com/byuoitav/raspi-deployment-microservice/master/docker-compose.yml --output /tmp/docker-compose.yml && curl " + os.Getenv("RASPI_DEPLOYMENT_MICROSERVICE_ADDRESS") + "/" + fileName + " --output /home/pi/.environment-variables && . ~/.bashrc && docker-compose -f /tmp/docker-compose.yml pull && docker stop -f $(docker ps -a -q) || true && docker rm -f $(docker ps -a -q) || true && docker-compose -f /tmp/docker-compose.yml up -d --remove-orphans'"
 
-	log.Print(longCommand)
+	log.Printf("Running the following command on %s: %s", hostname, longCommand)
 
 	err = magicSession.Run(longCommand)
 	if err != nil {
+		log.Printf("Error updating %s: %s", hostname, err.Error())
+		reportToELK(hostname, err)
 		return err
 	}
 
-	log.Print("Done.")
+	log.Printf("Finished updating %s", hostname)
 
 	return nil
 }
