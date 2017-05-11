166a8fd7c4395999cf6a23de7a28d37865b537f9
Joseph Blodgett <Joseph.blodgett@gmail.com>
2017-05-11 10:38:55 -0600

    Adding salt script

---
+++ salt-setup.sh
@@ -0,0 +1,14 @@
+wget -O - https://repo.saltstack.com/apt/debian/8/armhf/2016.11/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
+echo "deb http://repo.saltstack.com/apt/debian/8/armhf/2016.11 jessie main" | sudo tee --append /etc/apt/sources.list.d/saltstack.list
+sudo apt update
+sudo apt -Y install salt-minion 
+
+#Get the Minion Addr
+wget https://raw.githubusercontent.com/byuoitav/raspi-deployment-microservice/files/minion
+
+sed -i 's/\$SALT_MASTER_HOST/'$SALT_MASTER_HOST'/' minion
+sed -i 's/\$SALT_MASTER_FINGER/'$SALT_MASTER_FINGER'/' minion
+
+sudo mv minion /usr/etc/minion
+
+
