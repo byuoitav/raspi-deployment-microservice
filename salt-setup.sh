4d5a447037db2e94920e39c404f29489d8709c51
Joseph Blodgett <joseph.blodgett@gmail.com>
2017-05-22 16:38:17 -0600

    Update salt-setup.sh

--- salt-setup.sh
+++ salt-setup.sh
@@ -16,3 +16,9 @@ sudo mv minion /etc/salt/minion
 
 sudo setfacl -m u:pi:rwx /etc/salt/pki/minion/
 sudo setfacl -m u:pi:rwx /etc/salt/pki/minion/*
+
+sudo wget -o /usr/bin/ https://github.com/byuoitav/salt-event-proxy/releases/download/0.6/salt-event-proxy
+sudo chmod +x /usr/bin/salt-event-proxy
+sudo wget -o /usr/lib/systemd/system/ https://raw.githubusercontent.com/byuoitav/raspi-deployment-microservice/master/image/salt-event-proxy.service
+sudo systemctl enable salt-event-proxy
+sudo systemcetl start salt-event-proxy
