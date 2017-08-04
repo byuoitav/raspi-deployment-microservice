db167b4245eb0bd73c6539000b6b7559fbb8809c
dannyrandall <dgrandall@me.com>
2017-08-04 12:25:04 -0600

    screen shut off scripts

--- screenshutoff.sh
+++
@@ -1,8 +0,0 @@
-curl https://raw.githubusercontent.com/byuoitav/raspi-deployment-microservice/master/files/rpi-xssstart > /home/pi/xssstart 
-chmod +x /home/pi/xssstart
-
-export DISPLAY=:0
-
-./home/pi/xssstart curl -X PUT http://localhost:8888 &
-
-echo "Waiting for screenoff commands."
