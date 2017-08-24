fc8960e578309368eaf4543e2eb63541cb9c08e8
dannyrandall <dgrandall@me.com>
2017-08-24 09:36:33 -0600

    second boot script

--- image/first-boot.sh
+++ image/first-boot.sh
@@ -12,7 +12,7 @@ bootfile="/usr/local/games/firstboot"
 if [ -f "$bootfile" ]; then
 	echo "First boot."
 
-	# download pi-setup
+	# download pi-setup script
 	until $(curl https://raw.githubusercontent.com/byuoitav/raspi-deployment-microservice/master/pi-setup.sh > /tmp/pi-setup.sh); do
 		echo "Trying again."
 	done
@@ -25,50 +25,16 @@ if [ -f "$bootfile" ]; then
 else
 	echo "Second boot."
 
-	wait 45 
+	sleep 5
 	sudo chvt 2
 
-	until $(sudo usermod -aG docker pi); do
-		curl -sSL https://get.docker.com -k | sh
-		wait
-	done
-	echo "Added user pi to the docker group"
-
-	printf "Please trigger a build to get the necessary environment variables.\n"
-	printf "Waiting...\n"
-
-	# wait for env. variables
-	modtime=$(stat -c %Y /etc/environment)
-	printf "original mod time to /etc/environment: $modtime"
-	newtime=$(stat -c %Y /etc/environment)
-	until [ "$modtime" != "$newtime" ]; do
-		newtime=$(stat -c %Y /etc/environment)
-		printf "\tnew mod time to /etc/environment: $newtime\n"
-		sleep 10
-	done
-
-	printf "\nrecieved env. variables\n"
-	source /etc/environment
-
-	until $(curl https://raw.githubusercontent.com/byuoitav/raspi-deployment-microservice/master/mariadb-setup.sh > /tmp/mariadb-setup.sh); do
+	# download second-boot script
+	until $(curl https://raw.githubusercontent.com/byuoitav/raspi-deployment-microservice/master/second-boot.sh > /tmp/second-boot.sh); do
 		echo "Trying again."
 	done
-	chmod +x /tmp/mariadb-setup.sh
-	/tmp/mariadb-setup.sh
+	chmod +x /tmp/second-boot.sh
 
-	until $(curl https://raw.githubusercontent.com/byuoitav/raspi-deployment-microservice/master/salt-setup.sh > /tmp/salt-setup.sh); do
-		echo "Trying again."
-	done
-	chmod +x /tmp/salt-setup.sh
-
-	until [ -d "/etc/salt/" ]; do
-		/tmp/salt-setup.sh
-	done
-
-	until [ $(docker ps -q | wc -l) -gt 1 ]; do
-		echo "Waiting for docker containers to download"
-		sleep 10
-	done
+	/tmp/second-boot.sh
 
 	echo "Removing symlink to startup script."
 	sudo rm /usr/lib/systemd/system/default.target.wants/first-boot.service
@@ -78,7 +44,7 @@ clear
 printf "\n\n\n\n\n\n"
 printf "Setup complete! I'll never see you again."
 printf "\n\tPlease wait for me to reboot.\n"
-sleep 30
+sleep 20
 printf "\n\nBye lol"
 sleep 5
 
