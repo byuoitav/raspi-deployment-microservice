c44fda716bd40417b628456e06e7577e5e1f3e58
Daniel Randall <dgrandall@me.com>
2018-02-26 12:27:01 -0700

    log statements for resizing image

--- image/resizevar.sh
+++ image/resizevar.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
+echo "starting resize\n"
+
 # stop r/w on var partition
 init 1
 service dbus stop
 
+echo "stopped dbus\n"
+
 # unmount /var to resize it
 umount /var
+
+echo "unmounted var\n"
 
 fdisk /dev/mmcblk0p3 << EOF 
 p
@@ -19,11 +25,17 @@ p
 w
 EOF
 
+echo "finished fdisk\n"
+
 e2fsck /dev/mmcblk0p3
 resize2fs /dev/mmcblk0p3
 
+echo "fsck'ed and resized\n"
+
 # remount partition
 mount /dev/mmcblk0p3
+
+echo "remounted\n"
 
 # show mount table
 echo "done. mount table:\n"
