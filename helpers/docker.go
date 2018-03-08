8c4be0ab95f080634e5e46b1e3d728ea445ce9dc
Clark Green <clark_green@byu.edu>
2018-03-08 14:10:20 -0700

    removed A LOT of cruft

--- helpers/docker.go
+++ helpers/docker.go
@@ -1,23 +1,17 @@
 package helpers
 
 import (
-	"bufio"
-	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"regexp"
-	"strconv"
 	"strings"
 
 	"github.com/byuoitav/av-api/dbo"
 	"github.com/byuoitav/configuration-database-microservice/structs"
-	"github.com/fatih/color"
 )
 
 func GetDeviceDocker(target structs.Device) (string, error) {
@@ -61,105 +55,6 @@ func GetDeviceDocker(target structs.Device) (string, error) {
 	}
 
 	return fileName, nil
-}
-
-//builds a map of device IDs to docker-compose files
-func GetRoomDocker(room structs.Room, role string) (map[int]string, error) {
-
-	roleId, err := GetConfigDbRoleId(role)
-	if err != nil {
-		return nil, err
-	}
-
-	url := fmt.Sprintf("%s/configurations/rooms/%d/roles/%d", os.Getenv("DESIGNATION_MICROSERVICE_ADDRESS"), room.ID, roleId)
-
-	log.Printf("[helpers] making request against %s for room: %s", color.HiCyanString(url), color.HiGreenString(room.Name))
-
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		msg := fmt.Sprintf("cannot make new request: %s", err.Error())
-		log.Printf("%s", color.HiRedString("[helpers] %s", msg))
-		return nil, errors.New(msg)
-	}
-
-	err = SetToken(req)
-	if err != nil {
-		msg := fmt.Sprintf("failed to set bearer token: %s", err.Error())
-		log.Printf("%s", color.HiRedString("[helpers] %s", msg))
-		return nil, errors.New(msg)
-	}
-
-	var client http.Client
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		msg := fmt.Sprintf("non-200 response from designation microservice: %d", resp.StatusCode)
-		log.Printf("%s", color.HiRedString("[helpers] %s", msg))
-		return nil, errors.New(msg)
-	}
-
-	output := make(map[int]string)
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	finder := regexp.MustCompile("[0-9]+")
-
-	for _, piece := range bytes.Split(body, []byte("$$$")) {
-
-		rawId := finder.Find(piece)
-		if rawId == nil {
-			log.Printf("%s", color.HiRedString("unable to find device ID"))
-			continue
-		}
-
-		reader := bufio.NewReader(bytes.NewReader(piece))
-
-		_, err := reader.Discard(len(rawId) + 1) //	discard the id and the newline from the buffer
-		if err != nil {
-			log.Printf("%s", color.HiRedString("error discarding: %s", err.Error()))
-			continue
-		}
-
-		log.Printf("%s", color.HiMagentaString("rawId: %s", string(rawId)))
-
-		id, err := strconv.Atoi(string(rawId))
-		if err != nil {
-			return nil, err
-		}
-
-		fileName, err := GenerateRandomString(NUM_BYTES)
-		if err != nil {
-			return nil, err
-		}
-
-		fileLocation := os.Getenv("GOPATH") + DOCKER_PATH
-
-		outFile, err := os.OpenFile(fileLocation+fileName, os.O_RDWR|os.O_CREATE, 0777)
-		if err != nil {
-			return nil, err
-		}
-
-		_, err = io.Copy(outFile, reader)
-		if err != nil {
-			return nil, err
-		}
-
-		outFile.Close()
-		TrackFile(fileName, fileLocation)
-
-		output[id] = fileName
-
-	}
-
-	log.Printf("%v", output)
-
-	return output, nil
 }
 
 func GetConfigDbRoleId(role string) (int, error) {
--- helpers/s3.go
+++ helpers/s3.go
@@ -1,115 +1,15 @@
 package helpers
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/byuoitav/authmiddleware/bearertoken"
-	"github.com/byuoitav/configuration-database-microservice/structs"
 	"github.com/fatih/color"
 )
-
-//const FILE_NAME = "environment-variables"                       //name of file we use later
-const NUM_BYTES = 8
-const PORT = ":5001"                                            //port the designation microservice works on
-const ENDPOINT = "/configurations/designations/%d/%d/variables" //endpoint we use to make request against designation microservice
-const DOCKER_PATH = "/src/github.com/byuoitav/raspi-deployment-microservice/public/"
-const ENVIRO_PATH = "/src/github.com/byuoitav/raspi-deployment-microservice/public/"
-
-// retrieveEnvironmentVariables gets the environment variables for each Pi as a file to SCP over
-func retrieveEnvironmentVariables(room structs.Room, role string) (map[int]string, error) {
-
-	log.Printf("[helpers] fetching environment variables...")
-
-	roleId, err := GetRoleId(role)
-	if err != nil {
-		return nil, errors.New(fmt.Sprintf("invalid role or designation: %s", err.Error()))
-	}
-
-	response, err := MakeEnvironmentRequest(fmt.Sprintf("/environment/rooms/%d/roles/%d", room.ID, roleId))
-	if err != nil {
-		return nil, err
-	}
-
-	if response.StatusCode != http.StatusOK {
-		msg, err := ioutil.ReadAll(response.Body)
-		if err != nil {
-			return nil, errors.New(fmt.Sprintf("non-200 response from pi-designation-microservice: %d, unable to read response: %s", response.StatusCode, err.Error()))
-		}
-		return nil, errors.New(fmt.Sprintf("non-200 response from pi-designation-microservice: %d, message: %s", response.StatusCode, string(msg)))
-	}
-
-	body, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	environments := make(map[int]string)
-
-	err = json.Unmarshal(body, &environments)
-	if err != nil {
-		return nil, err
-	}
-
-	return environments, nil
-}
-
-func RetrieveDockerCompose(role, designation string) (string, error) {
-
-	log.Printf("[helpers] retrieving docker-compose file for devices of role: %s, designation: %s", role, designation)
-
-	//get role and designation IDs
-	roleID, desigId, err := GetClassAndDesignationID(role, designation)
-	if err != nil {
-		return "", errors.New(fmt.Sprintf("invalid role or designation: %s", err.Error()))
-	}
-
-	resp, err := MakeEnvironmentRequest(fmt.Sprintf("/configurations/designations/%d/%d/docker-compose", roleID, desigId))
-	if err != nil {
-		return "", err
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		return "", errors.New(fmt.Sprintf("non-200 response from pi-designation-microservice: %d", resp.StatusCode))
-	}
-
-	fileName, err := GenerateRandomString(NUM_BYTES)
-	if err != nil {
-		return "", err
-	}
-
-	fileLocation := os.Getenv("GOPATH") + DOCKER_PATH
-
-	outFile, err := os.OpenFile(fileLocation+fileName, os.O_RDWR|os.O_CREATE, 0777)
-	if err != nil {
-		return "", err
-	}
-
-	_, err = io.Copy(outFile, resp.Body)
-	if err != nil {
-		return "", err
-	}
-
-	outFile.Close()
-	TrackFile(fileName, fileLocation)
-
-	return fileName, nil
-}
-
-func SetDockerComposeFiles(rooms *map[int][]device) error {
-
-	return nil
-}
-
-func SetEnvironmentFiles(rooms *map[int][]device) error {
-	return nil
-}
 
 func GetClassAndDesignationID(role, designation string) (int64, int64, error) {
 
@@ -134,32 +34,6 @@ func GetClassAndDesignationID(role, designation string) (int64, int64, error) {
 	}
 
 	return roleId, desigId, nil
-}
-
-func MakeEnvironmentRequest(endpoint string) (*http.Response, error) {
-
-	var client http.Client
-
-	url := os.Getenv("DESIGNATION_MICROSERVICE_ADDRESS") + endpoint
-
-	log.Printf("[helplers] making request against url %s", url)
-
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return &http.Response{}, errors.New(fmt.Sprintf("unable to request docker-compose or etc/environment file: %s", err.Error()))
-	}
-
-	err = SetToken(req)
-	if err != nil {
-		return &http.Response{}, errors.New(fmt.Sprintf("unable to request docker-compose or etc/environment file: %s", err.Error()))
-	}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		return &http.Response{}, err
-	}
-
-	return resp, nil
 }
 
 func SetToken(request *http.Request) error {
