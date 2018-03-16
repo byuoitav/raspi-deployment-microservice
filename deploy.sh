a238fed445b1d337a1a9faf20bc00ef402450b50
Clark Green <clark_green@byu.edu>
2018-03-15 18:05:58 -0600

    moving to circle 2.0

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
-#!/usr/bin/env bash
-
 PROJECT_NAME=$1
 SHA1=$2 # Nab the SHA1 of the desired build from a command-line argument
+BRANCH=$3
 EB_BUCKET=elasticbeanstalk-us-west-2-194925301021
 
 # Create new Elastic Beanstalk version
@@ -10,7 +9,13 @@ sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json > $DOCKERRUN_FILE
 aws configure set default.region us-west-2
 aws configure set region us-west-2
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE # Copy the Dockerrun file to the S3 bucket
-aws elasticbeanstalk create-application-version --application-name $PROJECT_NAME --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
 
 # Update Elastic Beanstalk environment to new version
-aws elasticbeanstalk update-environment --environment-name $PROJECT_NAME-env --version-label $SHA1
+if [ "$BRANCH" == "master"]; then 
+	aws elasticbeanstalk create-application-version --application-name $PROJECT_NAME --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
+	aws elasticbeanstalk update-environment --environment-name $PROJECT_NAME-env --version-label $SHA1
+else
+	aws elasticbeanstalk create-application-version --application-name $PROJECT_NAME-stage --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
+	aws elasticbeanstalk update-environment --environment-name $PROJECT_NAME-stage --version-label $SHA1
+fi
+	
