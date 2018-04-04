4111dbc64f315778909222c7150c86d8fd48be88
Joseph Blodgett <Joseph.blodgett@gmail.com>
2018-04-04 13:20:39 -0600

    cleanup to make the first script run faster

--- pi-setup.sh
+++ pi-setup.sh
@@ -27,10 +27,6 @@ chmod +x /home/pi/changeroom.sh
 mkdir /home/pi/.i3
 curl https://raw.githubusercontent.com/byuoitav/raspi-deployment-microservice/master/files/i3_config > /home/pi/.i3/config
 
-# Install an ARM-specific Docker version
-curl -sSL http://downloads.hypriot.com/docker-hypriot_1.10.3-1_armhf.deb > /tmp/docker-hypriot_1.10.3-1_armhf.deb
-sudo sh -c "dpkg -i /tmp/docker-hypriot_1.10.3-1_armhf.deb; usermod -aG docker pi; systemctl enable docker.service"
-
 # Make X start on login
 curl https://raw.githubusercontent.com/byuoitav/raspi-deployment-microservice/master/files/bash_profile > /home/pi/.bash_profile
 
