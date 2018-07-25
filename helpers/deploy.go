a1abace2b59434dffd26e8967a1e438b1817b955
jpw547 <jwerner547@gmail.com>
2018-07-25 15:59:33 -0600

    switch branches in deploy-aws

--- helpers/deploy.go
+++ helpers/deploy.go
@@ -3,7 +3,6 @@ package helpers
 import (
 	"errors"
 	"fmt"
-	"log"
 	"os"
 	"strings"
 	"time"
@@ -87,7 +86,7 @@ func deployHelper(allDevices []structs.Device, class, designation string) ([]elk
 func Deploy(class, designation string) ([]elkReport, error) {
 	var report []elkReport
 
-	log.Printf("%s", color.HiGreenString("[helpers] deployment started"))                                      //scheduledDeployments[deploymentType] = false //why?? it seems like this code doesn't get executed if this line evaluates to true
+	l.L.Infof("%s", color.HiGreenString("[helpers] deployment started"))                                       //scheduledDeployments[deploymentType] = false //why?? it seems like this code doesn't get executed if this line evaluates to true
 	allDevices, er := db.GetDB().GetDevicesByRoleAndTypeAndDesignation("ControlProcessor", class, designation) //TODO: Make the deployment process role-dependent.
 	if er != nil {
 		return report, er
@@ -99,7 +98,7 @@ func Deploy(class, designation string) ([]elkReport, error) {
 func DeployBuilding(building, class, designation string) ([]elkReport, error) {
 	var report []elkReport
 
-	log.Printf("%s", color.HiGreenString("[helpers] deployment started"))                                      //scheduledDeployments[deploymentType] = false //why?? it seems like this code doesn't get executed if this line evaluates to true
+	l.L.Infof("%s", color.HiGreenString("[helpers] deployment started"))                                       //scheduledDeployments[deploymentType] = false //why?? it seems like this code doesn't get executed if this line evaluates to true
 	allDevices, er := db.GetDB().GetDevicesByRoleAndTypeAndDesignation("ControlProcessor", class, designation) //TODO: Make the deployment process role-dependent.
 	if er != nil {
 		return report, er
@@ -120,7 +119,7 @@ func DeployBuilding(building, class, designation string) ([]elkReport, error) {
 func DeployDevice(hostname string) (elkReport, error) {
 	var report elkReport
 
-	log.Printf("[helpers] starting single deployment...")
+	l.L.Infof("[helpers] starting single deployment...")
 
 	//hostname should be all caps - names in config DB are all caps
 	allCaps := strings.ToUpper(hostname)
@@ -129,19 +128,19 @@ func DeployDevice(hostname string) (elkReport, error) {
 	room, err := db.GetDB().GetRoom(hostname)
 	if err != nil {
 		msg := fmt.Sprintf("failed to get room: %s", err.Error())
-		log.Printf("%s", color.HiRedString("[helpers] %s", msg))
+		l.L.Infof("%s", color.HiRedString("[helpers] %s", msg))
 		return report, errors.New(msg)
 	}
 
 	//build device name
 	deviceName := strings.Split(allCaps, "-")[2]
-	log.Printf("[helpers] looking for device: %s", deviceName)
+	l.L.Infof("[helpers] looking for device: %s", deviceName)
 
 	//get device class
 	var deviceClass string
 	for _, device := range room.Devices {
 
-		log.Printf("[helpers] found device: %s of class: %s", device.Name, device.Type.ID)
+		l.L.Infof("[helpers] found device: %s of class: %s", device.Name, device.Type.ID)
 
 		if device.Name == deviceName { //found device
 
@@ -151,7 +150,7 @@ func DeployDevice(hostname string) (elkReport, error) {
 
 	if len(deviceClass) == 0 { //if we don't find anything
 		msg := "device class not found"
-		log.Printf("%s", color.HiRedString("[helpers] %s", msg))
+		l.L.Infof("%s", color.HiRedString("[helpers] %s", msg))
 		return report, errors.New(msg)
 	}
 
@@ -168,7 +167,7 @@ func DeployDevice(hostname string) (elkReport, error) {
 
 	dev, err := db.GetDB().GetDevice(hostname)
 	if err != nil {
-		log.Printf("error getting device")
+		l.L.Infof("error getting device")
 		return report, err
 	}
 
@@ -177,7 +176,7 @@ func DeployDevice(hostname string) (elkReport, error) {
 
 	report = <-respChan
 
-	log.Printf("deployment started")
+	l.L.Infof("deployment started")
 	return report, nil
 }
 
@@ -241,7 +240,7 @@ func SendCommand(hostname, environment, docker string, respChan chan elkReport) 
 	connection, err := ssh.Dial("tcp", hostname+":22", sshConfig)
 	if err != nil {
 		msg := fmt.Sprintf("Error dialing %s: %s", hostname, err.Error())
-		log.Printf(msg)
+		l.L.Infof(msg)
 
 		val := reportToELK(hostname, msg, false)
 		val.Success = false
@@ -249,13 +248,13 @@ func SendCommand(hostname, environment, docker string, respChan chan elkReport) 
 		return err
 	}
 
-	log.Printf("ssh connection established to %s", hostname)
+	l.L.Infof("ssh connection established to %s", hostname)
 	defer connection.Close()
 
 	magicSession, err := connection.NewSession()
 	if err != nil {
 		msg := fmt.Sprintf("error starting a session with %s: %s", hostname, err.Error())
-		log.Printf(msg)
+		l.L.Infof(msg)
 
 		val := reportToELK(hostname, msg, false)
 		val.Success = false
@@ -272,21 +271,21 @@ func SendCommand(hostname, environment, docker string, respChan chan elkReport) 
 		Timestamp: time.Now().Format(time.RFC3339),
 	}
 
-	log.Printf("SSH session established with %s", hostname)
+	l.L.Infof("SSH session established with %s", hostname)
 
 	longCommand := fmt.Sprintf("bash -c 'curl %s/%s --output /tmp/docker-compose-tmp.yml && curl %s/%s --output /home/pi/.environment-variables && curl %s/move-environment-variables.sh --output /home/pi/move-environment-variables.sh && chmod +x /home/pi/move-environment-variables.sh && /home/pi/move-environment-variables.sh && source /etc/environment && echo \"$(cat /tmp/docker-compose-tmp.yml)\" | envsubst > /tmp/docker-compose.yml && docker-compose -f /tmp/docker-compose.yml pull && docker stop $(docker ps -a -q) || true && docker rmi -f $(docker images -q --filter \"dangling=true\") || true && docker rm $(docker ps -a -q) || true && docker-compose -f /tmp/docker-compose.yml up -d' &> /tmp/deployment_logs.txt", os.Getenv("RASPI_DEPLOYMENT_MICROSERVICE_ADDRESS"), docker, os.Getenv("RASPI_DEPLOYMENT_MICROSERVICE_ADDRESS"), environment, os.Getenv("RASPI_DEPLOYMENT_MICROSERVICE_ADDRESS"))
 
-	log.Printf("Running the following command on %s: %s", hostname, longCommand)
+	l.L.Infof("Running the following command on %s: %s", hostname, longCommand)
 
 	err = magicSession.Run(longCommand)
 	if err != nil {
 		msg := fmt.Sprintf("%s", color.HiRedString("[helpers] error updating %s: %s", hostname, err.Error()))
-		log.Printf(msg)
+		l.L.Infof(msg)
 		reportToELK(hostname, msg, false)
 		return errors.New(msg)
 	}
 
-	log.Printf("%s", color.HiGreenString("[helpers] finished updating %s", hostname))
+	l.L.Infof("%s", color.HiGreenString("[helpers] finished updating %s", hostname))
 
 	reportToELK(hostname, "Command successfully executed.", true)
 
