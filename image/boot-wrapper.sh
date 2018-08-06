3e56f7bb5f62d0496f98b8334b9d95221378469c
Joseph Blodgett <Joseph.blodgett@gmail.com>
2018-08-06 15:44:40 -0600

    adding the boot wrapper

---
+++ image/boot-wrapper.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+printf "\n\ntarting run...\n\n"
+
+until $(curl https://raw.githubusercontent.com/byuoitav/raspi-deployment-microservice/master/pi-setup.sh > /tmp/first-boot.sh); do
+    echo "Cannot download first-boot"
+done
+
+chmod +x /tmp/first-boot.sh
+
+/tmp/firt-boot.sh | tee -a /etc/first-boot/logs.log
