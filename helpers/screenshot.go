d69b5b828565066e641194baea4345a1d4b9adf8
CalebLarsen <caleb.john.larsen@gmail.com>
2018-10-01 10:43:34 -0600

    Now we post the screenshot to a public AWS S3 bucket

--- helpers/screenshot.go
+++ helpers/screenshot.go
@@ -1,6 +1,7 @@
 package helpers
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -65,7 +66,7 @@ func init() {
 
 func MakeScreenshot(hostname string, address string) ([]byte, error) {
 	img := []byte{}
-	//Make our ssh client, writer, and session
+	//Make our ssh client, writer, and sesh
 
 	client, err := ssh.Dial("tcp", hostname+":22", sshConfiguration)
 	if err != nil {
@@ -73,19 +74,19 @@ func MakeScreenshot(hostname string, address string) ([]byte, error) {
 	}
 	output := socket.Writer(hostname)
 	if err != nil {
-		return img, nerr.Translate(err).Addf("Ssh session could not be opened")
+		return img, nerr.Translate(err).Addf("Ssh sesh could not be opened")
 	}
 
 	defer client.Close()
 	//Try to Open Session
-	session, er := NewSession(client, output)
+	sesh, er := NewSession(client, output)
 	if er != nil {
-		msg := fmt.Sprintf("unable to open session with %v: %v", hostname, er.Error())
+		msg := fmt.Sprintf("unable to open sesh with %v: %v", hostname, er.Error())
 		fmt.Fprintf(output, msg)
 		return img, er
 	}
 	//Try to Open (Warp) Pipe
-	stdin, err := session.StdinPipe()
+	stdin, err := sesh.StdinPipe()
 	if err != nil {
 		msg := fmt.Sprintf("unable to open stdin pipe on %v: %v", hostname, err)
 		fmt.Fprintf(output, msg)
@@ -93,7 +94,7 @@ func MakeScreenshot(hostname string, address string) ([]byte, error) {
 	}
 
 	//Try to Create a (Koopa) Shell
-	err = session.Shell()
+	err = sesh.Shell()
 	if err != nil {
 		msg := fmt.Sprintf("unable to start shell on %v: %v", output, err)
 		fmt.Fprintf(output, msg)
@@ -105,40 +106,45 @@ func MakeScreenshot(hostname string, address string) ([]byte, error) {
 	fmt.Fprintf(stdin, `script -f /tmp/screenshot.log`+"\n")
 	//Take the Screenshot
 	fmt.Fprintf(stdin, `xwd -out %s.xwd -root -display :0.0`+"\n", ScreenshotName)
-	//TODO -> Put this on AWS
+	//Puts the Screenshot onto AWS
 	fmt.Fprintf(stdin, `curl -XPOST https://byuoitav-raspi-deployment-microservice.avs.byu.edu/ReceiveScreenshot/%s -T ./%s.xwd`+"\n", ScreenshotName, ScreenshotName)
 	//Remove the Screenshot
 	fmt.Fprintf(stdin, `rm %s.xwd`+"\n", ScreenshotName)
 	fmt.Fprintln(stdin, `exit`)
 	stdin.Close()
-	err = session.Wait()
+	err = sesh.Wait()
 	if err != nil {
 		log.L.Warnf("failed to screenshot %v: %v", hostname, err)
 	}
 	//Convert the Screenshot to a .png
 	time.Sleep(250 * time.Millisecond)
 	FullScreenshotName := fmt.Sprintf("/tmp/%s.xwd", ScreenshotName)
-	cmd := exec.Command("convert", FullScreenshotName, "screenshot.png")
+	cmd := exec.Command("convert", FullScreenshotName, ScreenshotName+".png")
 	cmd.Run()
 	//	cmd = exec.Command("rm", FullScreenshotName)
 	//	cmd.Run()
 	//Read in the Screenshot
-	img, err = ioutil.ReadFile("screenshot.png")
+	//img, err = ioutil.ReadFile("screenshot.png")
 
 	if err != nil {
 		log.L.Infof("Failed to read Screenshot file %v: %v", ScreenshotName, err)
 	}
 
+	//TODO Put the screenshot into a public s3 bucket and then send that url back thus pulling the picture down to slack
+	svc := s3.New(session.New(), &aws.Config{Region: aws.String("us-west-2")})
+
+	_, err = svc.PutObject(&s3.PutObjectInput{
+		Bucket:        aws.String("byu-oit-av-screenshot-bucket"), //TODO CHANGE BUCKET
+		Key:           aws.String(ScreenshotName),
+		Body:          bytes.NewReader(img),
+		ContentLength: aws.Int64(int64(len(img))), //Size of Image
+		ContentType:   aws.String(".png"),
+	})
+
+	if err != nil {
+		log.L.Infof("Everything about Amazon has failed: %v", err)
+		return img, err
+	}
+
 	return img, nil
 }
-
-/*
-func MakeScreenshotGoToSlack(hostname, channelID string) error {
-
-	SSH INTO HOSTNAME
-	TAKE A SCREENSHOT AND CURL THAT TO SLACK(channelID)
-		IF THINGS DID NOT GO WELL, WE HAVE AN ERROR
-	RETURN I THINGS WENT A-OKAY
-}
-
-*/
