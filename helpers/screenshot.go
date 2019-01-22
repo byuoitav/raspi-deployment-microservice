8754fdf60ef0edc3b0cd1c3e6c3557ec8c964dae
CalebLarsen <caleb.john.larsen@gmail.com>
2019-01-22 16:16:58 -0700

    Reading the Slack response body

--- helpers/screenshot.go
+++ helpers/screenshot.go
@@ -29,78 +29,7 @@ type Message struct {
 // MakeScreenshot takes a screenshot on device and posts it to a slack channel
 func MakeScreenshot(hostname string, address string, userName string, outputChannelID string) error {
 	img := []byte{}
-	/*
-		//Make our ssh client, writer, and sesh
-		client, err := ssh.Dial("tcp", hostname+":22", sshConfig)
-		if err != nil {
-			return nerr.Translate(err).Addf("Client could not be created")
-		}
-		defer client.Close()
 
-		output := socket.Writer(hostname)
-		if err != nil {
-			return nerr.Translate(err).Addf("Ssh sesh could not be opened")
-		}
-
-		//Try to Open Session
-		sesh, er := NewSession(client, output)
-		if er != nil {
-			msg := fmt.Sprintf("unable to open sesh with %v: %v", hostname, er.Error())
-			fmt.Fprintf(output, msg)
-			return er
-		}
-
-		//Try to Open (Warp) Pipe
-		stdin, err := sesh.StdinPipe()
-		if err != nil {
-			msg := fmt.Sprintf("unable to open stdin pipe on %v: %v", hostname, err)
-			fmt.Fprintf(output, msg)
-			return err
-		}
-
-		//Try to Create a (Koopa) Shell
-		err = sesh.Shell()
-		if err != nil {
-			msg := fmt.Sprintf("unable to start shell on %v: %v", output, err)
-			fmt.Fprintf(output, msg)
-			return err
-		}
-
-		log.L.Debugf("Started new shell on %s", hostname)
-		//Our Screenshot name is a combination of the hostname and a timestamp to make it unique
-		ScreenshotName := hostname + "*" + time.Now().Format(time.RFC3339)
-		//Log our adventures on the pi
-		fmt.Fprintf(stdin, `script -f /tmp/screenshot.log`+"\n")
-		//Take the Screenshot
-		fmt.Fprintf(stdin, `xwd -out %s.xwd -root -display :0.0`+"\n", ScreenshotName)
-		//Puts the Screenshot onto AWS
-		fmt.Fprintf(stdin, `curl -XPOST https://byuoitav-raspi-deployment-microservice.avs.byu.edu/ReceiveScreenshot/%s -T ./%s.xwd`+"\n", ScreenshotName, ScreenshotName)
-		//Remove the Screenshot
-		fmt.Fprintf(stdin, `rm %s.xwd`+"\n", ScreenshotName)
-		fmt.Fprintln(stdin, `exit`)
-		//Close the Pipe and the Session
-		stdin.Close()
-		err = sesh.Wait()
-		if err != nil {
-			log.L.Warnf("failed to screenshot %v: %v", hostname, err)
-		}
-		//Convert the Screenshot to a .png with a moment to make sure that the screenshot is posted first
-		time.Sleep(250 * time.Millisecond)
-		FullScreenshotName := fmt.Sprintf("/tmp/%s.xwd", ScreenshotName)
-		cmd := exec.Command("convert", FullScreenshotName, ScreenshotName+".png")
-		err = cmd.Run()
-		if err != nil {
-			log.L.Errorf("Failed to execute convert command: %v", err.Error())
-		}
-
-		//Read in the Screenshot
-
-		img, err = ioutil.ReadFile(ScreenshotName + ".png")
-
-		if err != nil {
-			log.L.Infof("Failed to read Screenshot file %v: %v", ScreenshotName, err.Error())
-		}
-	*/
 	resp, err := http.Get("http://" + hostname + ":10000/device/screenshot")
 
 	if err != nil {
@@ -149,15 +78,15 @@ func MakeScreenshot(hostname string, address string, userName string, outputChan
 	}
 
 	//Marshal it
-	json, err := json.Marshal(message)
+	j, err := json.Marshal(message)
 	if err != nil {
 		log.L.Errorf("[Screenshot] failed to marshal message: %v", message)
 		return err
 	}
 
 	//Make the request
-	req, err := http.NewRequest("POST", "https://slack.com/api/chat.postMessage", bytes.NewBuffer(json))
-	req.Header.Set("Content-Type", "application/json")
+	req, err := http.NewRequest("POST", "https://slack.com/api/chat.postMessage", bytes.NewBuffer(j))
+	req.Header.Set("Content-type", "application/json")
 	req.Header.Set("Authorization", myToken)
 
 	slackClient := &http.Client{}
@@ -167,7 +96,19 @@ func MakeScreenshot(hostname string, address string, userName string, outputChan
 		log.L.Errorf("[Screenshot] We failed to send to Slack: %s", err.Error())
 	}
 
-	log.L.Warnf("[Screenshot] Slack Response: %v", resp)
+	log.L.Warnf("[Screenshot] Slack Response: %v", resp.Body)
+	var p []byte
+	_, err = resp.Body.Read(p)
+	if err != nil {
+		log.L.Errorf("[Screenshot] Couldn't read the Slack Response: %s", err.Error())
+	}
+	var i interface{}
+	err = json.Unmarshal(p, i)
+	if err != nil {
+		log.L.Errorf("[Screenshot] Couldn't unmarshal: %s", err.Error())
+	}
+
+	log.L.Warnf("[Screenshot] Unmarshalled body: %v", err.Error())
 
 	log.L.Infof("We made it to the end boys. It is done.")
 	return nil
