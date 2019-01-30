e7d02132c5d9d14f0fe69fdc6f8bf82253d438fd
CalebLarsen <caleb.john.larsen@gmail.com>
2019-01-30 10:25:10 -0700

    Changed log levels

--- helpers/screenshot.go
+++ helpers/screenshot.go
@@ -36,7 +36,7 @@ func MakeScreenshot(hostname string, address string, userName string, outputChan
 		log.L.Errorf("[Screenshot] We failed to get the screenshot: %s", err.Error())
 	}
 
-	log.L.Warnf("[Screenshot] Response: %v", resp)
+	log.L.Debugf("[Screenshot] Response: %v", resp)
 	ScreenshotName := hostname + "*" + time.Now().Format(time.RFC3339)
 
 	defer resp.Body.Close()
@@ -96,7 +96,7 @@ func MakeScreenshot(hostname string, address string, userName string, outputChan
 		log.L.Errorf("[Screenshot] We failed to send to Slack: %s", err.Error())
 	}
 
-	log.L.Warnf("[Screenshot] Slack Response: %v", resp.Body)
+	log.L.Debugf("[Screenshot] Slack Response: %+v", resp.Body)
 	var p []byte
 	_, err = resp.Body.Read(p)
 	if err != nil {
@@ -108,7 +108,7 @@ func MakeScreenshot(hostname string, address string, userName string, outputChan
 		log.L.Errorf("[Screenshot] Couldn't unmarshal: %s", err.Error())
 	}
 
-	log.L.Warnf("[Screenshot] Unmarshalled body: %v", err.Error())
+	log.L.Debugf("[Screenshot] Unmarshalled body: %+v", err.Error())
 
 	log.L.Infof("We made it to the end boys. It is done.")
 	return nil
